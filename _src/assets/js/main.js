8c877e22ebb8086bd769ca25c379540129285b82
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-22 18:32:21 +0200

    funciton to show title and image completed

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -11,12 +11,24 @@ buttonEl.addEventListener('click', handleSearchButton);
 // función que:
 function handleSearchButton () {
     // recoge en una constante el valor del input
-    const inputValue = inputEl.innerHTML;
-    // pinta en nuestro ul un li
+    const inputValue = inputEl.value;
+    // pinta en nuestro ul un li y lo meto en una constante
     ulEl.innerHTML = '<li class="list-item"></li>';
-    // dentro del li, mete un h2 y una imagen
+    const liEl = document.querySelector('.list-item');
+    // dentro del li, mete un h2 y una imagen y las guardo en constantes
+    liEl.innerHTML = '<h2 class="title"></h2><img class="photo" src="" alt="">';
+    const title = document.querySelector('.title');
+    const photo = document.querySelector('.photo');
     // hace una petición fetch a la url de la API: http://api.tvmaze.com/singlesearch/shows?q= + el valor del input, que:
+    fetch('http://api.tvmaze.com/singlesearch/shows?q=' + inputValue)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function(data) {
+        // console.log(data);
         // asigna al innerHTML del h2 el título de la serie
         // asigna al src de la imagen la url de la imagen
+        title.innerHTML = data.name;
+        photo.src = data.image.medium;
+    });
 }
-handleSearchButton();
