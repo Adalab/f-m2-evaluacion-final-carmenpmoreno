86f603b6f4c52302b32ce58994d81f811d23d4a4
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-23 21:53:23 +0200

    add mesagge and remove it if no imput value

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -12,7 +12,15 @@ function handleSearchButton() {
     const inputValue = inputEl.value;
     sectionEl.innerHTML = '<ul class="list"></ul>';
     const ulEl = document.querySelector('.list');
-    ulEl.innerHTML = '<li class="list-item"></li>';
+    ulEl.innerHTML = '<h3 class="message">¡Haz click si es de tus favoritas!</h2><li class="list-item"></li>';
+    
+    function removeMessage() {
+        if (!!!inputValue) {
+            ulEl.innerHTML = ' ';
+        };
+    }
+    removeMessage();
+
     const liEl = document.querySelector('.list-item');
     liEl.innerHTML = '<h2 class="title"></h2><img class="photo" src="" alt="">';
     const title = document.querySelector('.title');
@@ -23,7 +31,6 @@ function handleSearchButton() {
         })
         .then(function (data) {
             title.innerHTML = data.name;
-            console.log(data);
             if (data.image === null) {
                 photo.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
             } else {
@@ -40,18 +47,15 @@ function handleSearchButton() {
                 liEl.classList.add('list-item-select');
             };
         };
-        
         changeLiColor();
 
         const liElSelected = document.querySelectorAll('.list-item-select');
-        console.log(liElSelected);
         for (let i = 0; i < liElSelected.length; i++) {
             const newSelectedArray = selectedArray.push(liElSelected[i].innerHTML);
         }
-        sectionEl.innerHTML += '<ul class="selected-list"><h3 class="message">¡Prueba a buscar alguna más!</h2>';
+        sectionEl.innerHTML += '<ul class="selected-list">';
         const SelectedList = document.querySelector('.selected-list');
         SelectedList.innerHTML = '<h2 class="selected-list__title">Mis series favoritas</h2>';
-        console.log(SelectedList);
         SelectedList.innerHTML += selectedArray;
     }
 }
