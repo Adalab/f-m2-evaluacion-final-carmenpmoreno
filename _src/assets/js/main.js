ea5d311dad9b51aaeb2766cc220926b2980bd899
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-26 10:45:10 +0200

    fix error

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -53,10 +53,10 @@ function handleSearchButton() {
             console.log(selectedArray);
         }
         
-        // sectionEl.innerHTML += '<ul class="selected-list">';
-        // const SelectedList = document.querySelector('.selected-list');
-        // SelectedList.innerHTML = '<h2 class="selected-list__title">Mis series favoritas</h2>';
-        // SelectedList.innerHTML += selectedArray;
+        sectionEl.innerHTML += '<ul class="selected-list">';
+        const SelectedList = document.querySelector('.selected-list');
+        SelectedList.innerHTML = '<h2 class="selected-list__title">Mis series favoritas</h2>';
+        SelectedList.innerHTML += selectedArray;
 
         saveCache(selectedArray);
     };
